96a7ddfa86334e4c213a1351652934c32300e28c
Ryan Duffy <ryan@replay.io>
2023-07-25 15:52:16 -0700

    metrics

--- e2e/runner/cypress-runner-run-tests.js
+++ e2e/runner/cypress-runner-run-tests.js
@@ -1,15 +1,15 @@
-const cypress = require("cypress");
+// const cypress = require("cypress");
 
 const {
   executeYarnCommand,
-  parseArguments,
+  // parseArguments,
   args,
 } = require("./cypress-runner-utils");
 
 const folder = args["--folder"];
-const isFolder = !!folder;
+// const isFolder = !!folder;
 
-const isOpenMode = args["--open"];
+// const isOpenMode = args["--open"];
 
 const getSourceFolder = folder => {
   return `./e2e/test/scenarios/${folder}/**/*.cy.spec.js`;
@@ -21,40 +21,47 @@ const runCypress = async (baseUrl, exitFunction) => {
     message: "Removing the existing Cypress artifacts\n",
   });
 
-  const defaultConfig = {
-    browser: "chrome",
-    configFile: "e2e/support/cypress.config.js",
-    config: {
-      baseUrl,
-    },
-    spec: isFolder && getSourceFolder(folder),
-  };
+  // const defaultConfig = {
+  //   browser: "chrome",
+  //   configFile: "e2e/support/cypress.config.js",
+  //   config: {
+  //     baseUrl,
+  //   },
+  //   spec: isFolder && getSourceFolder(folder),
+  // };
 
-  const userArgs = await parseArguments(args);
+  // const userArgs = await parseArguments(args);
 
-  const finalConfig = Object.assign({}, defaultConfig, userArgs);
+  // const finalConfig = Object.assign({}, defaultConfig, userArgs);
 
   try {
-    const { status, message, totalFailed, failures } = isOpenMode
-      ? await cypress.open(finalConfig)
-      : await cypress.run(finalConfig);
+    await executeYarnCommand({
+      command: `npx @replayio/cypress --mode diagnostic --spec ${getSourceFolder(
+        folder,
+      )} --config baseurl=${baseUrl} --config-file e2e/support/cypress.config.js --browser replay-chromium`,
+      message: "Running cypress tests with replay.io runner\n",
+    });
+
+    // const { status, message, totalFailed, failures } = isOpenMode
+    //   ? await cypress.open(finalConfig)
+    //   : await cypress.run(finalConfig);
 
     // At least one test failed, so let's generate HTML report that helps us determine what went wrong
-    if (totalFailed > 0) {
-      await executeYarnCommand({
-        command: "yarn run generate-cypress-html-report",
-        message: "Generating Mochawesome HTML report\n",
-      });
+    // if (totalFailed > 0) {
+    //   await executeYarnCommand({
+    //     command: "yarn run generate-cypress-html-report",
+    //     message: "Generating Mochawesome HTML report\n",
+    //   });
 
-      await exitFunction(1);
-    }
+    //   await exitFunction(1);
+    // }
 
     // Something went wrong and Cypress failed to even run tests
-    if (status === "failed" && failures) {
-      console.error(message);
+    // if (status === "failed" && failures) {
+    //   console.error(message);
 
-      await exitFunction(failures);
-    }
+    //   await exitFunction(failures);
+    // }
   } catch (e) {
     console.error("Failed to run Cypress!\n", e);
 
