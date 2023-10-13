e3ba32950b44567ad2d02ab00151cc69f15f7161
Dan Miller <dan@replay.io.>
2023-10-13 15:13:24 -0700

    download replay browser to specific directory

--- .buildkite/run-fly-tests.sh
+++ .buildkite/run-fly-tests.sh
@@ -8,6 +8,19 @@ cleanup() {
 
 yarn install
 
+# TODO(dmiller): download Replay browser
+# set RECORD_REPLAY_DIRECTORY to current working directory + .replay
+RECORD_REPLAY_DIRECTORY=`pwd`/.replay
+
+# create the .replay directory
+mkdir -p $RECORD_REPLAY_DIRECTORY/runtimes
+
+# download the Replay browser to RECORD_REPLAY_DIRECTORY/runtimes/Replay-Chromium.app/
+curl -L https://static.replay.io/downloads/macOS-chromium-20230916-e7589a401ac1-4d0a9f5b9de2.dmg -o $RECORD_REPLAY_DIRECTORY/runtimes/Replay-Chromium.app.dmg
+hdiutil attach $RECORD_REPLAY_DIRECTORY/runtimes/Replay-Chromium.app.dmg
+cp -R /Volumes/Replay-Chromium/Replay-Chromium.app $RECORD_REPLAY_DIRECTORY/runtimes/Replay-Chromium.app
+hdiutil detach /Volumes/Replay-Chromium
+
 # This will ensure that the cleanup function is called when the script exits, regardless of the exit status.
 trap cleanup EXIT
 
