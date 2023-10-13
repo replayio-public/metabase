3d960728d30ea5ebe2dc2b8d1b1576eb9f202c47
Dan Miller <dan@replay.io.>
2023-10-13 11:20:19 -0700

    add some more missing flags

--- .buildkite/run-fly-tests.sh
+++ .buildkite/run-fly-tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # start the fly app
-fly deploy -a replay-mb-${BUILDKITE_BUILD_NUMBER} -c fly.toml
+fly deploy -a replay-mb-${BUILDKITE_BUILD_NUMBER} -c fly.toml --vm-size shared-cpu-4x --ha false -y
 
 # run the tests
-CYPRESS_REPLAYIO_ENABLED=1 E2E_HOST=https://replay-mb-${BUILDKITE_BUILD_NUMBER}-.dev QA_DB_ENABLED=false yarn test-cypress-run --e2e --browser replay-chromium
+CYPRESS_REPLAYIO_ENABLED=1 E2E_HOST=https://replay-mb-${BUILDKITE_BUILD_NUMBER}.dev QA_DB_ENABLED=false yarn test-cypress-run --e2e --browser replay-chromium
 
 # destroy the fly app
 fly apps destroy -a replay-mb-${BUILDKITE_BUILD_NUMBER} -y
