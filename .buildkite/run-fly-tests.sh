bb846e2d7a0744cab29bcdc8bbd476374e26eb10
Dan Miller <dan@replay.io.>
2023-10-13 10:50:33 -0700

    Use replay browser

--- .buildkite/run-fly-tests.sh
+++ .buildkite/run-fly-tests.sh
@@ -4,7 +4,7 @@
 fly deploy -a replay-mb-${BUILDKITE_BUILD_NUMBER} -c fly.toml
 
 # run the tests
-E2E_HOST=https://replay-mb-${BUILDKITE_BUILD_NUMBER}-.dev QA_DB_ENABLED=false yarn test-cypress-run --e2e
+CYPRESS_REPLAYIO_ENABLED=1 E2E_HOST=https://replay-mb-${BUILDKITE_BUILD_NUMBER}-.dev QA_DB_ENABLED=false yarn test-cypress-run --e2e --browser replay-chromium
 
 # destroy the fly app
 fly apps destroy -a replay-mb-${BUILDKITE_BUILD_NUMBER} -y
