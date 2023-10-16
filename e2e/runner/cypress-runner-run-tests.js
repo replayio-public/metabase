10971ac7fec4825c1dfce4c96be30b8e7a1095ce
Dan Miller <dan@replay.io.>
2023-10-16 10:06:33 -0700

    remove thing that doesn't seem to work on mac out of the box

--- e2e/runner/cypress-runner-run-tests.js
+++ e2e/runner/cypress-runner-run-tests.js
@@ -40,14 +40,15 @@ const runCypress = async (baseUrl, exitFunction) => {
       : await cypress.run(finalConfig);
 
     // At least one test failed, so let's generate HTML report that helps us determine what went wrong
-    if (totalFailed > 0) {
-      await executeYarnCommand({
-        command: "yarn run generate-cypress-html-report",
-        message: "Generating Mochawesome HTML report\n",
-      });
+    // NOTE(dmiller): this doesn't seem to work on mac, commenting out
+    // if (totalFailed > 0) {
+    //   await executeYarnCommand({
+    //     command: "yarn run generate-cypress-html-report",
+    //     message: "Generating Mochawesome HTML report\n",
+    //   });
 
-      await exitFunction(1);
-    }
+    //   await exitFunction(1);
+    // }
 
     // Something went wrong and Cypress failed to even run tests
     if (status === "failed" && failures) {
